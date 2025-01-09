fe2227de97085af53f02151378484b8fd6b96cdf
Muhammad-Ammad3 <ammad0598@gmail.com>
2025-01-10 02:00:31 +0500

    first commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+
+const Header = ()=>{
+    return(
+    <header className="py-8 xl:py-12 text-white ">
+    <div className="container mx-auto">
+        <Link></Link>
+    </div>
+    </header>
+    )
+}
+
+
+
+export default Header
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	conatiner : {
+		center : "true",
+		padding : "15px",
+	},
+	screens : {
+		sm : "640px",
+		md : "768px",
+		lg : "960",
+		xl : "1200",
+	},
+	fontFamily : {
+		primary : "var(--font-jetbarinsMono)",
+	},
+  	extend: {
+		keyframes : {
+			"accordtion-down":{
+				from : {height : "0"},
+				to : {height : "var(--radix-accordtion-content-height"},
+			},
+			"accordtion-up":{
+				from : {height : "var(--radix-accordtion-content-height"},
+				to : {height : "0"},
+			},
+		},
+		animation : {
+			"accordtion-down" :"accordtion-down 0.2 ease-out",
+			"accordtion-up" :"accordtion-up 0.2 ease-out",
+		},
+  		colors: {
+			primary : "#1c1c22",
+			accent : {
+				DEFAULT : "#00ff99",
+				hover : "#00e187"
+			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
