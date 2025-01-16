017d3167c443cbf41d5d7ab1eecbb5727371c2e8
Muhammad-Ammad3 <ammad0598@gmail.com>
2025-01-17 00:37:38 +0500

    updated code

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -44,87 +44,40 @@ const projects = [
 ]
 
 const Work = () => {
-    const [project , setProject] = useState(projects[0])
-
-    const handleSlideChange = (swiper) => {
-        // get current slide index 
-        const currentIndex = swiper.activeIndex;
-        // update project state baseed on current slide index 
-        setProject(projects[currentIndex])
-    }
-
-    return(
-        <motion.section initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
-            <div className="container mx-auto">
-                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-                    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-                        <div className="flex flex-col gap-[30px] h-[50%]">
-                            {/* outline num */}
-                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
-                                {project.num}
-                            </div>
-                            {/* project category  */}
-                            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} Project</h2>
-                            {/* project description  */}
-                            <p className="text-white/60">{project.description}</p>
-                            {/* stack  */}
-                            <ul className="flex gap-4">
-                                {project.stack.map((item,index) => {
-                                    return(
-                                        <li key={index} className="text-xl text-accent">{item.name}
-                                        {/* remove the last comma  */}
-                                        {index !== project.stack.length -1 && ","}</li>
-                                    )
-                                })}
-                            </ul>
-                            {/* border */}
-                            <div className="border border-white/20"></div>
-                            {/* buttons */}
-                            <div className="flex items-center gap-4">
-                                {/* live project button */}
-                                <Link href={project.live}>
-                                <TooltipProvider delayDuration={100}>
-                                    <Tooltip>
-                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsArrowUpRight className="text-white text=3xl group-hover:text-accent" />
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>Live Project</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                </Link>
-                                {/* github project button  */}
-                                <Link href={project.github}>
-                                <TooltipProvider delayDuration={100}>
-                                    <Tooltip>
-                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsGithub className="text-white text=3xl group-hover:text-accent" />
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>Github Repository</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full xl:w-[50%]">
-                        <Swiper>
-                            {projects.map((project, index) => {
-                                return(
-                                    <SwiperSlide key={index}>slide</SwiperSlide>
-                                )
-                            })}
-                        </Swiper>
-                    </div>
+   const [project , setProject] = useState(projects[0]);
+   return(
+    <motion.section initial={{opacity: 0}} animate={{opacity: 1}}
+    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+        <div className="container mx-auto">
+            <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+            <div className="w-full xl:w-[50%] xl:h-[420px] flex flex-col xl:justify-between order-2 xl:order-none">
+                <div>
+                {/* outline num */}
+                <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
                 </div>
+                {/* project category  */}
+                <div>
+                    <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} Project</h2>
+                    {/* project description  */}
+                    <p className="text-white/60">{project.description}</p>
+                    {/* stack  */}
+                    <ul className="flex gap-4">
+                        {project.stack.map((item, index) => {
+                            return(
+                                <li key={index} className="text-xl text-accent">{item.name}
+                                {/* remove the last comma  */} 
+                                {index !== project.stack.length -1 && ","}</li>
+                            )
+                        } 
+                        )}
+                    </ul>
+                </div>
+                </div>   
+            <div className="w-full xl:w-[50%]">slider</div>   
             </div>
-        </motion.section>
-    )
+        </div>
+    </motion.section>
+   )
 } 
 
 
